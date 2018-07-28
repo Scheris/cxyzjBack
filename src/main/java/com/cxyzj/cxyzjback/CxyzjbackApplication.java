fa37c91500f162d6923d73404fbfbb410840dcf7
wanyu <335767798@qq.com>
2018-07-28 20:51:51 +0800

    Initial commit

---
+++ src/main/java/com/cxyzj/cxyzjback/CxyzjbackApplication.java
@@ -0,0 +1,12 @@
+package com.cxyzj.cxyzjback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxyzjbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CxyzjbackApplication.class, args);
+    }
+}
---
+++ src/test/java/com/cxyzj/cxyzjback/CxyzjbackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cxyzj.cxyzjback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CxyzjbackApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
