d7142bdf2ea0ec941774b71ad4a4024a80f7d76b
wanyu <335767798@qq.com>
2018-08-04 21:12:40 +0800

    添加test

--- src/test/java/com/cxyzj/cxyzjback/CxyzjbackApplicationTests.java
+++ src/test/java/com/cxyzj/cxyzjback/CxyzjbackApplicationTests.java
@@ -1,17 +1,47 @@
 package com.cxyzj.cxyzjback;
 
-import com.cxyzj.cxyzjback.Bean.user.User;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 @SpringBootTest
 @Slf4j
 public class CxyzjbackApplicationTests {
+    private MockMvc mockMvc; // 模拟MVC对象，通过MockMvcBuilders.webAppContextSetup(this.context).build()初始化。
+
+    @Autowired
+    private WebApplicationContext context; // 注入WebApplicationContext
+
+    @Before // 在测试开始前初始化工作
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+    }
+
+    /**
+     * @Author yaser
+     * @desc 测试接口 获取用户列表
+     */
     @Test
-    public void contextLoads() {
-        User user = new User();
-        log.info(user.getClass().getSimpleName().toLowerCase());
+    public void testQ1() throws Exception {//接口测试
+        String res = mockMvc.perform(MockMvcRequestBuilders.get("/v1/front/users")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn().getResponse().getContentAsString();
+        log.info(res);
     }
 }
